0450b5bae97587a11bdbe72db0ab46cc5199104e
Yamileth195 <20118803@clases.edu.sv>
2025-03-02 17:58:06 -0600

    segunndo commit y push

--- 3ASP20118803/form2.aspx.cs
+++ 3ASP20118803/form2.aspx.cs
@@ -15,7 +15,26 @@ namespace _3ASP20118803
         {
             // Retrieve the session variables.
             ddlCategory.SelectedValue = Session["ddlCategory"].ToString();
-           
+            ddlSupplier.SelectedValue = Session["ddlSupplier"].ToString();
+            lblProduct.Text = Session["strProduct"].ToString();
+            txtDescription.Text = Session["strDescription"].ToString();
+            lblImage.Text = Session["strImage"].ToString();
+            Decimal decPrice =
+                Convert.ToDecimal(Session["decPrice"]);
+            lblPrice.Text = decPrice.ToString("c");
+            lblNumberInStock.Text = Session["bytNumberInStock"].ToString();
+            lblNumberOnOrder.Text = Session["bytNumberOnOrder"].ToString();
+            lblReorderLevel.Text = Session["bytReorderLevel"].ToString();
+            Byte bytNumberInStock =
+                Convert.ToByte(Session["bytNumberInStock"]);
+            Byte bytNumberOnOrder =
+                Convert.ToByte(Session["bytNumberOnOrder"]);
+
+            Decimal deValueInStock = decPrice * bytNumberInStock;
+            Decimal deValueOnOrder = decPrice * bytNumberOnOrder;
+            lblNumberInStock.Text = deValueInStock.ToString("c");
+            lblNumberOnOrder.Text = deValueOnOrder.ToString("c");
+
 
         }
     }
